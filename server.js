62f285d89d92c63eabf82afec71b038b5ce03aa7
Liz Chen <elizab14@elizabethchen.com>
2020-04-03 20:47:01 -0400

    update

--- server.js
+++ server.js
@@ -5,27 +5,36 @@ const axios = require('axios');
 const port = process.env.PORT || 3100;
 const puppeteer = require('puppeteer');
 
-(async () => {
-    const browser = await puppeteer.launch({
-        headless: true,
-        args: [
-            '--no-sandbox',
-            '--proxy-server="direct://"',
-            '--proxy-bypass-list=*',
-            '--disable-dev-shm-usage',
-            '--single-process'
-        ]
-    });
-    const page = await browser.newPage();
-    await page.goto('https://news.ycombinator.com', {waitUntil: 'networkidle2'});
-    await page.pdf({path: 'hn.pdf', format: 'A4'});
-
-    await browser.close();
-})();
+// (async () => {
+//     const browser = await puppeteer.launch({
+//         headless: true,
+//         args: [
+//             '--no-sandbox',
+//             '--proxy-server="direct://"',
+//             '--proxy-bypass-list=*',
+//             '--disable-dev-shm-usage',
+//             '--single-process'
+//         ]
+//     });
+//     const page = await browser.newPage();
+//     await page.goto('https://news.ycombinator.com', {waitUntil: 'networkidle2'});
+//     await page.pdf({path: 'hn.pdf', format: 'A4'});
+//
+//     await browser.close();
+// })();
 
 const getPage = function(){
     return new Promise(async function(resolve, reject){
-        const browser = await puppeteer.launch();
+        const browser = await puppeteer.launch({
+            headless: true,
+            args: [
+                '--no-sandbox',
+                '--proxy-server="direct://"',
+                '--proxy-bypass-list=*',
+                '--disable-dev-shm-usage',
+                '--single-process'
+            ]
+        });
         const page = await browser.newPage();
         await page.goto('http://www.instagram.com/bobafettcav/', {waitUntil: 'networkidle2'});
         // await page.pdf({path: 'hn.pdf', format: 'A4'});
